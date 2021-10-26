d9301743085b4aad0fbbc03cd9258c09892ecae7
Matthias Diener <mdiener@illinois.edu>
2021-10-26 15:34:43 -0500

    fix tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,14 +3,14 @@
 mkdir ..\build-stage
 cd ..\build-stage
 
-cmake -G "NMake Makefiles" ^
-      -DCMAKE_BUILD_TYPE="Release"                 ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"             ^
-      -DCMAKE_INSTALL_LIBDIR="lib" ^
-      -DBUILD_SHARED_LIBS=ON                       ^
-      -DENABLE_DOCS=OFF                            ^
-      -DENABLE_EXAMPLES=ON                         ^
-      -DENABLE_TESTS=OFF                           ^
+cmake -G "NMake Makefiles"                           ^
+      -DCMAKE_BUILD_TYPE="Release"                   ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_LIBDIR="lib                    ^
+      -DBUILD_SHARED_LIBS=ON                         ^
+      -DENABLE_DOCS=OFF                              ^
+      -DENABLE_EXAMPLES=ON                           ^
+      -DENABLE_TESTS=OFF                             ^
       %SRC_DIR%
 
 if errorlevel 1 exit 1
